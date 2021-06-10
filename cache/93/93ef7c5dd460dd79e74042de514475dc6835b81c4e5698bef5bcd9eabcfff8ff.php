e187ddb9ea2afd83e89ad11696b92ade026ba0a8
Mikhail Mikhailov <mikhail.mikhailov@icloud.com>
2021-06-10 14:18:54 +0000

    First Commit

---
+++ cache/93/93ef7c5dd460dd79e74042de514475dc6835b81c4e5698bef5bcd9eabcfff8ff.php
@@ -0,0 +1,54 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* hello.twig */
+class __TwigTemplate_d898f4fc459a5cf10e3685ef608ce5cf14f512f86fbedc5ff74053d2a598ae74 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "ufgh";
+    }
+
+    public function getTemplateName()
+    {
+        return "hello.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "hello.twig", "/var/www/templates/hello.twig");
+    }
+}
